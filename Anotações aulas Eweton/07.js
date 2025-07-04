849acd3425cebfa871db890b0c97399ff2565077
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-04 12:54:15 -0300

    Asplpsl

--- Anotações aulas Eweton/07.js
+++ Anotações aulas Eweton/07.js
@@ -1,9 +1,22 @@
+const elias = +document.getElementById('trtrtrt').value;
+
+
+const Tia = "Mãe do Elias";
+
 const carro = {
     ano:5,
     modelo: "Gol" ,
-    marca: "Volks"
+    marca: "Volks",
+    filho : "Golf",
+    pai : "Gol quadrado",
+    o_melor_carro : `Fusca da ${Tia}`
 }
 
-for(let ano in carro){
-    console.log(ano);
+for(let Elias in carro){
+    console.log(Elias +': '+ carro[Elias]);
 }
+
+
+let oi = 2;
+
+oi = "vai tomar no cu";
