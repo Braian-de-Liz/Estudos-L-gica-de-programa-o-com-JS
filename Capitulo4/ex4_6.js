d40220f8d953f423a34c8cde9bd4d0df593e7e77
Braian-de-Liz <delizbraian@gmail.com>
2025-07-08 15:15:06 -0300

    elias Resgistradoral

---
+++ Capitulo4/ex4_6.js
@@ -0,0 +1,42 @@
+const form = document.querySelector('form');
+const Resp1 = document.getElementById("Resp1");
+const Resp2 = document.getElementById("Resp2");
+const Resp3 = document.getElementById("Resp3");
+
+
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const Saque = +document.getElementById("Numero").value;
+
+
+    if(Saque % 10 != 0){
+        alert('elias elias elias elias elias elias elias elias elias elias elias elias');
+        Saque.focus();
+        return;
+    }
+
+    const NotasDCem = Math.floor(Saque/100);
+    let Resto = Saque % 100;
+
+
+    const Notas50 = Math.floor(Resto/50);
+    Resto = Resto % 50;
+
+    const Nota10 = Math.floor(Resto/10);
+
+    if(NotasDCem > 0){
+        Resp1.innerText = `Temos ${NotasDCem} Notas de R$100`;
+        Resp1.style.color = "green"
+    }
+
+    if(Notas50 > 0){
+        Resp2.innerText = `E ${Notas50} notas de R$50`;
+        Resp2.style.color = "blue"
+    }
+
+    if(Nota10 > 0){
+        Resp3.innerText = `E isso aqui de ${Nota10} Notas de R$10`;
+        Resp3.style.color = "red"
+    }
+});
