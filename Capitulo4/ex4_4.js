ef64a4e4cb1ff24600630902fa7fd02503cfa671
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-08 08:10:50 -0300

    hyhcftcyh

---
+++ Capitulo4/ex4_4.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+const Resp = document.getElementById('Resp1');
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const HoraBrasil = +document.getElementById('HoraBrasil').value;
+    let HoraFranca = HoraBrasil + 5;
+
+    if(HoraFranca > 24){
+        HoraFranca = HoraFranca - 24;
+    }
+
+    Resp.innerText = `Hora Agora na França é ${HoraFranca.toFixed(2)}`
+
+
+});
