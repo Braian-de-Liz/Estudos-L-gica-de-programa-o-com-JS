09d5010d1ae60cafb291eb3f90a05448d23f2181
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-10 10:15:04 -0300

    we

---
+++ Capitulo4/ex4_7.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+const pessoas = +prompt('Numero de pessoas');
+const peixes = +prompt('Numero de peixes');
+
+
+let pagar
+
+if(peixes <= pessoas){
+    
+}
