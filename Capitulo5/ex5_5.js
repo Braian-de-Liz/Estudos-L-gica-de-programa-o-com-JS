0692850354abf4d979202affca355460c125c7b4
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-20 18:14:34 -0300

    edededddedededed

---
+++ Capitulo5/ex5_5.js
@@ -0,0 +1,31 @@
+/* const prompt = require('prompt-sync')();
+
+let numero = +prompt("digite numero");
+
+for(let i = 1; i<=10; i++){
+    console.log(`${numero} x ${i} - ${numero * i}`);
+} */
+
+const form = document.querySelector("form");
+const Resp1 = document.getElementById("Resp1");
+const Resp2 = document.getElementById("Resp2");
+
+let resposta = "";
+let numcontas = 0;
+let valorFI = 0;
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const Descricao = document.getElementById("Descricao")
+    const Valor = document.getElementById("Valor_Pagar").value;
+
+    numcontas++;
+    valorFI += Valor;
+
+    resposta = resposta + Descricao + "R$:" + Valor.toFixed(2)+"\n";
+
+
+    Resp1.innerText = `${resposta} -------------------------`;
+    Resp2.innerText = `${numcontas}`
+});
