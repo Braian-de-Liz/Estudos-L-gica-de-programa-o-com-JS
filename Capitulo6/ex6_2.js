0d4428a4c3540b4c3698dbadaeafe6b7dcdf27a4
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-26 20:14:54 -0300

    Elias gosta

---
+++ Capitulo6/ex6_2.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("#Formulario");
+const RespChances = document.getElementById("out_Chances");
+const RespErros = document.getElementById("outErros");
+const RespDica = document.getElementById("Dica");
+
+const erros = [];
+const sorteio = Math.floor(Math.random() * 100) + 1;
+const chances = 6;
+
+function dicacao(numeroSorteio, numeroUser) {
+    const Dica = numeroSorteio < numeroUser ? "menor" : "maior";
+    RespDica.innerText = `Tente um número ${Dica} que ${numeroUser} `;
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const numero = +form.numero.value;
+
+    if (numero === sorteio) {
+        RespDica.innerText = `Parabéns! Número sorteado: ${sorteio}`;
+        form.Submetedor.disabled = true;
+        form.btNovo.className = "exibe";
+    } 
+    else {
+        if (erros.includes(numero)) {
+            alert(`Você já tentou o número ${numero}`);
+        } 
+        else {
+            erros.push(numero);
+            const NumChances = chances - erros.length;
+            
+            dicacao(sorteio, numero);
+            
+            RespErros.innerText = `${erros.length} (${erros.join(",")})`;
+            RespChances.innerText = `Chances: ${NumChances}`;
+
+            if (NumChances === 0) {
+                form.Submetedor.disabled = true;
+                form.btNovo.className = "exibe";
+                RespDica.innerText = `Game Over! O número era ${sorteio}`;
+            }
+        }
+    }
+    
+    form.numero.value = "";
+    form.numero.focus();
+});
