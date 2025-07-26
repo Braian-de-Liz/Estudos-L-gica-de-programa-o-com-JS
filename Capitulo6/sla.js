74038bb51e7ba5f546dbafbc33bfb35f7195f630
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-26 20:10:07 -0300

    Elias está adivinhando números

--- Capitulo6/sla.js
+++ Capitulo6/sla.js
@@ -1,5 +1,12 @@
-const frutas = ['maçã', 'banana', 'laranja', 'elias'];
+/* const frutas = ['maçã', 'banana', 'laranja', 'elias'];
 
 frutas.forEach((fruta, posicao) => {
   console.log(`${posicao + 1}. ${fruta.toUpperCase()}`);
-});
+}); */
+
+
+let elias = 0.8;
+
+elias = Math.floor(elias);
+
+console.log(elias);
