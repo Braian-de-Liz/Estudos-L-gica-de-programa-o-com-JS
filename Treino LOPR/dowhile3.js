6bd8588143f2a56f156b14bb23c145e3bd93a054
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-29 19:08:11 -0300

    ELIAS GOSTA MUITO DE LÓGICA

--- Treino LOPR/dowhile3.js
+++ Treino LOPR/dowhile3.js
@@ -1,12 +1,24 @@
 const prompt = require("prompt-sync")();
-let sair = false;
-sair = prompt("você quer sair responda SIM/NÃO");
+
+let nota;
+let valorNota = 0;
+let sair = prompt("Você quer sair SIM/NÃO").toUpperCase();
+let contador = 0;
 
 do {
-    let nota = +prompt("me diga uma nota, ");
-    console.log("está era sua nota " + nota)
+    contador++
+    nota = +prompt("Diga uma nota");
+    valorNota += nota;
+    sair = prompt("quer adicionar mais? SIM/NÃO").toUpperCase();
 
-    if(sair === "SIM"){
-        
-    }
-} while (sair = false);
+
+} while (sair === "SIM");
+
+if (contador > 0) {
+    let media = valorNota / contador;
+    console.log(`A média das notas é ${media}`);
+} else {
+    console.log(    );
+}
+
+
