d16625d226e5a76ebf1a0df492811b8d3112ef98
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-08-05 23:03:59 -0300

    teste de elias

--- Treino LOPR/zzzz.cs
+++ Treino LOPR/zzzz.cs
@@ -1,4 +1,15 @@
-int num2 = 21;
-int num1 = 34;
+using System;
 
-console.writeLine(num1 + num2);
+class Principal
+{
+    static void Main()
+    {
+
+        int num2 = 21;
+        int num1 = 34;
+
+        console.writeLine(num1 + num2);
+
+    }
+}
+
