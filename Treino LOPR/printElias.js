2954a288ab6435486eebcb4ca8ec4a23e70e2908
Braian-de-Liz <delizbraian@gmail.com>
2025-08-08 11:38:40 -0300

    çjkrghfghdhgfhdoasduyisvhidfhfvhxnopiYR76236RGQ WHJ-9E48TW ¨&¨%$¨#%u4587UDUT*¨¨%rz|4U6YN56ÇKNB NKLNCOKSDMVÇLUHIUEUPRUGLKWEQRKLEWIOJF9DK´W9KDWI5JTLÇREKJGÇLKDMVIOAERYFGHISHC

---
+++ Treino LOPR/printElias.js
@@ -0,0 +1,44 @@
+function printa(variravel) {
+    console.log(variravel);
+}
+
+function gerarNome() {
+    const nomes = [
+        "João",
+        "Maria",
+        "Pedro",
+        "Ana",
+        "Lucas",
+        "Julia",
+        "Gabriel",
+        "Laura",
+        "Matheus",
+        "Isabella",
+        "Rafael",
+        "Sophia",
+        "Felipe",
+        "Manuela",
+        "Daniel",
+        "Alice",
+        "Marcos",
+        "Valentina",
+        "Gustavo",
+        "Helena"
+    ]
+}
+
+const aleatorio = Math.floor(Math.random() * 10);
+
+const elias = "elias";
+let eliasi = 0;
+
+
+printa(elias);
+
+
+
+
+
+
+
+
--- Treino LOPR/teste.py
+++ Treino LOPR/teste.py
@@ -1,12 +1,12 @@
 def printaVar(variavel):
     print(variavel);
-elias = [10, 9, 4]
+elias = [10, 9, 4];
 soma = 0
 
 for i in range(len(elias)):
-    soma += elias[i]
+    soma += elias[i];
 
 comprimento = len(elias);
-media = soma / comprimento
+media = soma / comprimento;
 
 printaVar(elias);
