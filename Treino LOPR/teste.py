50121372845da2cc923e908c4fd4e9226196f8df
Braian-de-Liz <delizbraian@gmail.com>
2025-08-08 09:44:15 -0300

    fkdhbkdfbkdsfbkjfdbvjfndvlfaçvçlerjgierutpqrweutpwerout

--- Treino LOPR/teste.py
+++ Treino LOPR/teste.py
@@ -1,11 +1,12 @@
-elias = [1, 9, 4]
+def printaVar(variavel):
+    print(variavel);
+elias = [10, 9, 4]
 soma = 0
 
 for i in range(len(elias)):
     soma += elias[i]
 
-comprimento = len(elias)
+comprimento = len(elias);
 media = soma / comprimento
 
-print("A média do vetor é", media)
-
+printaVar(elias);
